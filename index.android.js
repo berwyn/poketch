f858bb062be2c1f549d6328174751fcd945e17ae
berwyn <berwyn.codeweaver@gmail.com>
2016-04-02 00:40:31 -0600

    Remove React-Native

--- index.android.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-import React, { AppRegistry } from 'react-native';
-import { Poketch } from './android/components/MainComponent';
-
-AppRegistry.registerComponent('poketch', () => Poketch);
